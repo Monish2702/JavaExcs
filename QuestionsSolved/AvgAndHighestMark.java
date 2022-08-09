eb470490dc54989cef6f25e681b097fe33f0e3ad
Monish2702 <gsmkumar2002@gmail.com>
2022-08-09 17:17:48 +0530

    excs

--- QuestionsSolved/AvgAndHighestMark.java
+++ QuestionsSolved/AvgAndHighestMark.java
@@ -6,30 +6,31 @@ import java.util.Scanner;
 
 public class AvgAndHighestMark {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double mark_avg;
-        int result;
-        int i;
-        int s;
-        // define size of array
-        s = input.nextInt();
-        // The array is defined "arr" and inserted marks into it.
-        int[] arr = new int[s];
-        for (i = 0; i < arr.length; i++) {
-            arr[i] = input.nextInt();
-        }
-        int max = 0;
-        mark_avg = 0;
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i] > max)
-                max = arr[i];
-            mark_avg += arr[i];
-        }
-        result = max;
-        mark_avg /= arr.length;
+        try (Scanner input = new Scanner(System.in)) {
+            double mark_avg;
+            int result;
+            int i;
+            int s;
+            // define size of array
+            s = input.nextInt();
+            // The array is defined "arr" and inserted marks into it.
+            int[] arr = new int[s];
+            for (i = 0; i < arr.length; i++) {
+                arr[i] = input.nextInt();
+            }
+            int max = 0;
+            mark_avg = 0;
+            for (i = 0; i < arr.length; i++) {
+                if (arr[i] > max)
+                    max = arr[i];
+                mark_avg += arr[i];
+            }
+            result = max;
+            mark_avg /= arr.length;
 
-        System.out.println(result);
-        System.out.print(mark_avg);
-        input.close();
+            System.out.println(result);
+            System.out.print(mark_avg);
+            input.close();
+        }
     }
 }
