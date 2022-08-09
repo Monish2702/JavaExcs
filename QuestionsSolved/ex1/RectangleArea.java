ef091514c69967a171100b15c8f861ce82958ad9
Monish2702 <gsmkumar2002@gmail.com>
2022-08-09 15:29:43 +0530

    excs

---
+++ QuestionsSolved/ex1/RectangleArea.java
@@ -0,0 +1,38 @@
+// Write a program to print the area of a rectangle by creating a class named 'Area' having two methods. 
+// First method named as 'setDim' takes length and breadth of rectangle as parameters 
+// and the second method named as 'getArea' returns the area of the rectangle.
+// Length and breadth of rectangle are entered through keyboard.
+
+package QuestionsSolved.ex1;
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Area area = new Area();
+        System.out.println("Enter length: ");
+        int length = sc.nextInt();
+        System.out.println("Enter breadth: ");
+        int breadth = sc.nextInt();
+        area.setDim(length, breadth);
+        System.out.println("Area of rectangle is: " + area.getArea());
+        sc.close();
+    }
+}
+
+class Area{
+    private int length, breadth;
+    void setDim(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getBreadth(){
+        return breadth;
+    }
+    public int getArea(){
+        return length * breadth;
+    }
+}
---
+++ QuestionsSolved/ex2/StudentRollNo.java
@@ -0,0 +1,37 @@
+// Create a class named 'Student' with String variable 'name' and integer variable 'roll_no'. 
+// Assign the value of roll_no as '2' and that of name as "John" by creating an object of the class Student.
+package QuestionsSolved.ex2;
+
+import java.util.Scanner;
+
+public class StudentRollNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter roll no: ");
+        int roll_no = sc.nextInt();
+        Student student = new Student(name, roll_no);
+        System.out.println("Name: " + student.getName());
+        System.out.println("Roll no: " + student.getRollNo());
+        sc.close();
+    }
+}
+
+class Student {
+    private String name;
+    private int roll_no;
+
+    public Student(String name, int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNo() {
+        return roll_no;
+    }
+}
