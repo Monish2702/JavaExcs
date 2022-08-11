acce58f72870fdfc8e70d0d5c406b057cce3adc3
Monish2702 <gsmkumar2002@gmail.com>
2022-08-11 12:21:43 +0530

    commit

--- Encrypt/src/Encryption.java
+++ Encrypt/src/Encryption.java
@@ -1,20 +1,16 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Encryption {
     private Scanner cin;
-    private Random random;
     private ArrayList<Character> list;
     private ArrayList<Character> shuffledList;
     private char character;
-    private char[] letters; //the scapegoat message
-    private char[] secretLetters;
+    private char[] letters; // the scapegoat message
+
     Encryption() {
         cin = new Scanner(System.in);
-        random = new Random();
         list = new ArrayList<Character>();
         shuffledList = new ArrayList<Character>();
         character = ' ';
@@ -24,30 +20,34 @@ public class Encryption {
     }
 
     private void askChoice() {
-        System.out.println("\n1.Encrypt\n2.Decrypt\n3.Generate new key\n4.Get key\n5.Exit");
-        System.out.print("\nEnter your choice: ");
-        int choice = cin.nextInt();
-        switch (choice) {
-            case 1:
-                encrypt();
-                break;
-            case 2:
-                decrypt();
-                break;
-            case 3:
-                newKey();
-                break;
-            case 4:
-                getKey();
-                break;
-            case 5:
-                exit();
-                break;
-            default:
-                System.out.println("\nInvalid choice!");
-                askChoice();
+        while (true) {
+            System.out.println("\n1.Encrypt\n2.Decrypt\n3.Generate new key\n4.Get key\n5.Exit");
+            System.out.print("\nEnter your choice: ");
+            int choice = cin.nextInt();
+            cin.nextLine();
+            switch (choice) {
+                case 1:
+                    encrypt();
+                    break;
+                case 2:
+                    decrypt();
+                    break;
+                case 3:
+                    newKey();
+                    break;
+                case 4:
+                    getKey();
+                    break;
+                case 5:
+                    exit();
+                    break;
+                default:
+                    System.out.println("\nInvalid choice!");
+                    askChoice();
+            }
         }
     }
+
     private void newKey() {
         character = ' ';
         list.clear();
@@ -57,25 +57,27 @@ public class Encryption {
             list.add(Character.valueOf(character));
             character++;
         }
-        
+
         shuffledList = new ArrayList(list);
-        Collections.shuffle(shuffledList); 
+        Collections.shuffle(shuffledList);
     }
-    private void getKey(){
-        //now here
+
+    private void getKey() {
+        // now here
         System.out.println("\nThe key is: ");
-        for(Character c : list){
+        for (Character c : list) {
             System.out.print(c);
         }
         System.out.println();
-        for(Character c : shuffledList){
+        for (Character c : shuffledList) {
             System.out.print(c);
         }
         System.out.println();
     }
+
     private void encrypt() {
         System.out.println("\nEnter the message to be encrypted: ");
-        String message = cin.next();
+        String message = cin.nextLine();
         letters = message.toCharArray();
         System.out.println("\nThe encrypted message is: ");
         for (int i = 0; i < letters.length; i++) {
@@ -86,16 +88,33 @@ public class Encryption {
                 }
             }
         }
-        for(char c : letters){
+        for (char c : letters) {
             System.out.print(c);
         }
         System.out.println();
-        askChoice();
     }
 
     private void decrypt() {
+        System.out.println("\nEnter the message to be decrypted: ");
+        String message = cin.next();
+        letters = message.toCharArray();
+        System.out.println("\nThe decrypted message is: ");
+        for (int i = 0; i < letters.length; i++) {
+            for (int j = 0; j < shuffledList.size(); j++) {
+                if (letters[i] == shuffledList.get(j)) {
+                    letters[i] = list.get(j);
+                    break;
+                }
+            }
+        }
+        for (char c : letters) {
+            System.out.print(c);
+        }
+        System.out.println();
     }
 
     private void exit() {
+        System.out.println("\nExiting...");
+        System.exit(0);
     }
 }
